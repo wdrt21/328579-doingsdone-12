af5e80e7b229055f16533021bf56b926f6e04a40
wdrt21 <vadiart@yandex.ru>
2021-10-26 13:03:18 +0700

    Внес исправления в schema.sql

--- schema.sql
+++ schema.sql
@@ -1,37 +1,46 @@
-CREATE DATABASE todo
+CREATE DATABASE IF NOT EXISTS todo
    DEFAULT CHARACTER SET utf8
    DEFAULT COLLATE utf8_general_ci;
 
 USE todo;
 
-CREATE TABLE projects (
-                     id INT AUTO_INCREMENT PRIMARY KEY,
-                     name CHAR(128) NOT NULL ,
-                     user_id INT NOT NULL
-);
+CREATE TABLE IF NOT EXISTS projects (
+                                      id INT AUTO_INCREMENT PRIMARY KEY,
+                                      name VARCHAR(255) NOT NULL ,
+                                      user_id INT NOT NULL
 
-CREATE TABLE task (
-                       id INT AUTO_INCREMENT PRIMARY KEY,
-                       user_id INT NOT NULL,
-                       proeject_id INT NOT NULL,
-                       dt_add TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-                       status BOOL DEFAULT 0,
-                       title CHAR(255) NOT NULL,
-                       file_path CHAR(255),
-                       data_exp DATE NULL,
-                       INDEX(user_id),
-                       INDEX(title),
-                       INDEX(data_exp)
-);
+  );
 
-CREATE TABLE users (
-                      id INT AUTO_INCREMENT PRIMARY KEY,
-                      dt_reg TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-                      email VARCHAR(128) NOT NULL UNIQUE,
-                      name CHAR(128) NOT NULL,
-                      password CHAR(64) NOT NULL,
-                      INDEX(name)
+CREATE TABLE IF NOT EXISTS task (
+                                  id INT AUTO_INCREMENT PRIMARY KEY,
+                                  user_id INT NOT NULL,
+                                  proeject_id INT NOT NULL,
+                                  dt_add TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                                  status BOOL DEFAULT 0,
+                                  title VARCHAR(255) NOT NULL,
+                                  file_path VARCHAR(255),
+                                  data_exp DATE NULL,
+                                  INDEX(user_id),
+                                  INDEX(title),
+                                  INDEX(data_exp)
+
+  );
+
+CREATE TABLE IF NOT EXISTS users (
+                                   id INT AUTO_INCREMENT PRIMARY KEY,
+                                   dt_reg TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                                   email VARCHAR(128) NOT NULL UNIQUE,
+                                   name VARCHAR(255) NOT NULL,
+                                   password VARCHAR(255) NOT NULL,
+                                   INDEX(name)
 
 
-)
+
+  );
+
+ALTER TABLE projects
+  ADD FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE;
+ALTER TABLE task
+  ADD FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE,
+	ADD FOREIGN KEY (proeject_id) REFERENCES projects (id) ON DELETE CASCADE;
 
