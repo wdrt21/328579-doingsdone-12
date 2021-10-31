0516b35b51cb76f0c1af642ce024b9e2008e5ebd
wdrt21 <vadiart@yandex.ru>
2021-10-31 18:15:35 +0700

    сделал подзапрос выборки данных

--- queries.sql
+++ queries.sql
@@ -1,13 +1,5 @@
 USE todo;
 
-# Добавление названий проектов
-
-INSERT INTO projects (name,user_id)
-VALUES ('Входящие', 1),
-       ('Учеба', 1),
-       ('Работа', 1),
-       ('Домашние дела', 2),
-       ('Авто', 2);
 
 # Добавление пользователей
 
@@ -16,18 +8,27 @@ INSERT INTO users
 VALUES ('loss@gmail.com', 'Дмитрий', 'Y734e9sdfg'),
        ('leopold@gmail.com', 'Александр', '1234567');
 
+# Добавление названий проектов
+
+INSERT INTO projects (name,user_id)
+VALUES ('Входящие', (SELECT id FROM users u WHERE u.email = 'loss@gmail.com')),
+       ('Учеба', (SELECT id FROM users u WHERE u.email = 'loss@gmail.com')),
+       ('Работа', (SELECT id FROM users u WHERE u.email = 'loss@gmail.com')),
+       ('Домашние дела', (SELECT id FROM users u WHERE u.email = 'leopold@gmail.com')),
+       ('Авто', (SELECT id FROM users u WHERE u.email = 'leopold@gmail.com'));
+
 
 # Добавление задач
 
 
 INSERT INTO task
   (title, data_exp, proeject_id, user_id, status)
-VALUES ('Собеседование в IT компании', STR_TO_DATE('06.10.2021', '%d.%m.%Y'), 6, 1, 0),
-       ('Выполнить тестовое задание', STR_TO_DATE('05.10.2021', '%d.%m.%Y'), 7, 1, 0),
-       ('Сделать задание первого раздела', STR_TO_DATE('21.12.2021', '%d.%m.%Y'), 8, 2, 1),
-       ('Встреча с другом', STR_TO_DATE('22.12.2021', '%d.%m.%Y'), 9, 1, 0),
-       ('Купить корм для кота', NULL, 7, 2, 0),
-       ('Заказать пиццу', NULL, 10, 1, 0);
+VALUES ('Собеседование в IT компании', STR_TO_DATE('06.10.2021', '%d.%m.%Y'), (SELECT id FROM projects p WHERE p.name = 'Входящие'), (SELECT id FROM users u WHERE u.email = 'loss@gmail.com'), 0),
+       ('Выполнить тестовое задание', STR_TO_DATE('05.10.2021', '%d.%m.%Y'), (SELECT id FROM projects p WHERE p.name = 'Входящие'), (SELECT id FROM users u WHERE u.email = 'loss@gmail.com'), 0),
+       ('Сделать задание первого раздела', STR_TO_DATE('21.12.2021', '%d.%m.%Y'), (SELECT id FROM projects p WHERE p.name = 'Учеба'), (SELECT id FROM users u WHERE u.email = 'loss@gmail.com'), 1),
+       ('Встреча с другом', STR_TO_DATE('22.12.2021', '%d.%m.%Y'), (SELECT id FROM projects p WHERE p.name = 'Работа'), (SELECT id FROM users u WHERE u.email = 'leopold@gmail.com'), 0),
+       ('Купить корм для кота', NULL, (SELECT id FROM projects p WHERE p.name = 'Домашние дела'), (SELECT id FROM users u WHERE u.email = 'leopold@gmail.com'), 0),
+       ('Заказать пиццу', NULL, (SELECT id FROM projects p WHERE p.name = 'Авто'), (SELECT id FROM users u WHERE u.email = 'leopold@gmail.com'), 0);
 
 
 # Получить список из всех проектов для одного пользователя
